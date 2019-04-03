49a9b79e82f1fd4d003b2aea3605a3df434b318e
hwong <hcwong2@illinois.edu>
2019-04-03 01:45:08 -0700

    spark consumer code and new yaml

---
+++ project/spark/consumerSpark.py
@@ -0,0 +1,19 @@
+import os
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 pyspark-shell'
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import json
+
+sc = SparkContext(appName="AminerKafkaSpark")
+scstrm = StreamingContext(sc,120)
+
+#Defining Kafka Consumer
+kafkaStream = KafkaUtils.createStream(scstrm,'localhost:2181','spark-streaming1',{'foobar':1})
+
+#Extract the content
+parsed = kafkaStream.map(lambda v:json.loads(v[1]))
+
+pageids = parsed.map(lambda paper: paper['id'])
+
+pageids.pprint(10)
