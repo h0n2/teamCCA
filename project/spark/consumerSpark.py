f40c0ea4f449d8d882a07fbfc8260d13337effc3
hwong <hcwong2@illinois.edu>
2019-04-03 02:05:41 -0700

    spark code bug fix - resolve package issue

--- project/spark/consumerSpark.py
+++ project/spark/consumerSpark.py
@@ -1,5 +1,5 @@
 import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 pyspark-shell'
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 pyspark-shell'
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
