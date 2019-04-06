b77fa895334f4431174318a0ca99502130e918fb
hwong <hcwong2@illinois.edu>
2019-04-06 11:57:19 -0700

    new spark consumer

--- project/spark/consumerSpark.py
+++ project/spark/consumerSpark.py
@@ -1,15 +1,13 @@
-import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 pyspark-shell'
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+from pyspark.streaming.kafka import kafkaUtils
 import json
 
 sc = SparkContext(appName="AminerKafkaSpark")
 scstrm = StreamingContext(sc,120)
 
 #Defining Kafka Consumer
-kafkaStream = KafkaUtils.createStream(scstrm,'localhost:2181','spark-streaming1',{'foobar':1})
+kafkaStream = kafkaUtils.createStream(scstrm,'localhost:2181','spark-streaming1',{'foobar':1})
 
 #Extract the content
 parsed = kafkaStream.map(lambda v:json.loads(v[1]))
@@ -17,3 +15,5 @@ parsed = kafkaStream.map(lambda v:json.loads(v[1]))
 pageids = parsed.map(lambda paper: paper['id'])
 
 pageids.pprint(10)
+
+return scstrm
