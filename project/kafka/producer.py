8ef0ad4f2fb13dce2cad1c3efe744c3275f47ce6
Ubuntu <digimine@datapod3.hpjkav4aev4uxo2jbflk0tzx1d.xx.internal.cloudapp.net>
2019-04-18 01:56:43 +0000

    read file lists for producer code; bug fix on env setting in sparkConsumer

--- project/kafka/producer.py
+++ project/kafka/producer.py
@@ -1,32 +1,43 @@
 #  Core Kafka Producer script for generating the Messages
 #  Authored: hcwong2@illinois.edu, manaskm2@illinois.edu, fmc2@illinois.edu, skusuma3@illinois.edu
 
+#from pip._internal import main as pipmain
+#pipmain(['install','kafka-python'])
+
+
 from kafka import KafkaProducer
 import time
 import json
+import os
 
+
+folder = r"data/"
 producer = KafkaProducer(bootstrap_servers='localhost:9092', request_timeout_ms=1000000, api_version_auto_timeout_ms=1000000)
-filename = r"data\aminer_papers_0.txt"
+files = filter(lambda  x: x[-4:] == '.txt', os.listdir(folder))
+#filename = r"data\aminer_papers_0.txt"
 cnt =0
-with open(filename, encoding="UTF-8") as json_file:
-    for line_number, line in enumerate(json_file):
-        #if line_number <= 100:
-            if cnt >=5:
-                print("sleeping ...")
-                time.sleep(15)
-                cnt = 0
-            else:
-                # Load the Json and construct the new json
-                jsonObj = json.loads(line)
-                try:
-                    del jsonObj['abstract']
-                    newLine = json.dumps(jsonObj)
-                except KeyError:
-                    newLine = line
+for filename in files:
+    with open((folder + filename), encoding="UTF-8") as json_file:
+        for line_number, line in enumerate(json_file):
+            #if line_number <= 100:
+                if cnt >=5:
+                    print("sleeping ...")
+                    time.sleep(15)
+                    cnt = 0
+                else:
+                    # Load the Json and construct the new json
+                    jsonObj = json.loads(line)
+                    try:
+                        del jsonObj['abstract']
+                        newLine = json.dumps(jsonObj)
+                    except KeyError:
+                        newLine = line
 
-                print(newLine)
-                producer.send('aminer', bytes(newLine,'utf-8'))
-                cnt = cnt + 1
+                    print(newLine)
+                    producer.send('aminer', bytes(newLine,'utf-8'))
+                    cnt = cnt + 1
+
+        os.rename((folder + filename), (folder + filename[:-4]+'.prs'))
         #else:
         #    break
 
