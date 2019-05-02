c4a310a39c339ee273cbc4a0bd097678e7c3505c
hcwong2 <digimine@aiserv01.hsd1.wa.comcast.net>
2019-05-01 19:45:32 -0700

    expose 4040 port for Spark; added batch mode and round robin topic distro

---
+++ batch_mode/consumerSpark.py
@@ -0,0 +1,42 @@
+#  Core Kafka Consumer for consuming Kafka messages, integrating with Neo4j
+#  Authored: hcwong2@illinois.edu, manaskm2@illinois.edu, fmc2@illinois.edu, skusuma3@illinois.edu
+
+from __future__ import print_function
+
+import os
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 pyspark-shell'
+
+from pip._internal import main as pipmain 
+pipmain(['install','py2neo', 'kafka-python'])
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import json
+import PreparePub
+
+sc = SparkContext(appName="AminerKafkaSpark")
+ssc = StreamingContext(sc,5)  # 10 seconds 
+
+#Defining Kafka Consumer
+# consumer1 <- Consumer Instance(Consumer GroupID)
+# foobar <- Topic
+# 1 <- Per topic kafka partitions to consume
+# Say for example you have a topic named "Topic1" with 2 partitions and you have provided the option 'Topic1':1, 
+#  then Kafka receiver will read 1 partition at a time [It will eventually read all the partitions but will read one partition at a time]. 
+#  The reason for this is to read the messages in partition and also preserving the order in which the data is written to the topic.
+
+kafkaStream = KafkaUtils.createStream(ssc,'zookeeper:2181','consumer1',{'aminer0':1})
+
+def processRecord(record):
+	#print(type(record))  #pyspark.rdd.RDD
+	newRecord = record.collect()
+	for x in newRecord:
+		PreparePub.formatNodes(json.loads(x))
+	print('******************COMPLETE********************')
+
+lines = kafkaStream.map(lambda x:x[1])
+lines.foreachRDD(processRecord)
+
+ssc.start()
+ssc.awaitTermination()
---
+++ batch_mode/producer_batch.py
@@ -0,0 +1,52 @@
+#  Core Kafka Producer script for generating the Messages
+#  Authored: hcwong2@illinois.edu, manaskm2@illinois.edu, fmc2@illinois.edu, skusuma3@illinois.edu
+
+#from pip._internal import main as pipmain
+#pipmain(['install','kafka-python'])
+
+
+from pip._internal import main as pipmain 
+pipmain(['install','kafka-python'])
+
+from kafka import KafkaProducer
+import time
+import json
+import os
+
+
+folder = r"data/"
+producer = KafkaProducer(bootstrap_servers='localhost:9092', request_timeout_ms=1000000, api_version_auto_timeout_ms=1000000)
+files = filter(lambda  x: x[-4:] == '.txt', os.listdir(folder))
+#filename = r"data\aminer_papers_0.txt"
+batchSize = 50000
+cnt=0
+topic=0
+for filename in files:
+    with open((folder + filename), encoding="UTF-8") as json_file:
+        print('### Processing starts for',filename,'###')
+        for line_number, line in enumerate(json_file):
+            if cnt > batchSize:
+                print("Waiting to send next batch...")
+                time.sleep(5)
+                cnt = 0
+                topic= 1 if topic == 0 else 0
+            
+            # Load the Json and construct the new json
+            jsonObj = json.loads(line)
+            try:
+                del jsonObj['abstract']
+                newLine = json.dumps(jsonObj)
+            except KeyError:
+                newLine = line
+
+            print(str(line_number),newLine)
+            topix = 'aminer'+str(topic)
+            print(topix)
+            producer.send(topix, bytes(newLine,'utf-8'))
+            cnt = cnt + 1
+
+        print('### Processing completed for',filename,'###')
+        os.rename((folder + filename), (folder + filename[:-4]+'.prs'))
+        time.sleep(5)
+
+producer.close()
